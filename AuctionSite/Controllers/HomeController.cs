9e394b580f590ceb9d1770f8a246a8d87f912749
ruca6616 <ruca6616@gmail.com>
2019-10-23 14:18:05 -0700

    Fixed syntax issue

--- AuctionSite/Controllers/HomeController.cs
+++ AuctionSite/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace AuctionSite.Controllers
     {
         public IActionResult Index()
         {
-            return View();oij
+            return View();
         }
 
         public IActionResult Privacy()
